62f4f14a0cc883b0bf456e392a1125ff28c95569
mpsalunggg <putrasatria893@gmail.com>
2024-09-12 22:33:02 +0700

    konstanta

---
+++ 3-Konstanta/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	const firstName string = "john"
+	fmt.Print("halo ", firstName, "!\n")
+
+	const name = "Putra"
+	const age = 20
+
+	// fmt print
+	fmt.Print("Hai nama saya ", name, " dan umur saya ", age, "\n")
+
+	// fmt println
+	fmt.Println("halooo banggg")
+}
