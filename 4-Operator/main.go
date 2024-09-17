e7bac76a5d3cbed8cc94c67b1b0f53086a6eb042
mpsalunggg <putrasatria893@gmail.com>
2024-09-17 22:50:26 +0700

    materi operator

---
+++ 4-Operator/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Operator Arithmetic 
+	const value = (((2 + 2) % 2) * 2 - 2) / 2
+	fmt.Println(value)
+
+	// Operator Comparison
+	// == -> equal
+	// != -> not equal
+	// etc same like js
+
+	var num1 = 1 
+	var num2 = 1
+
+	var andOperator = num1 + num1 == 2 && num1 + num2 == 1
+	var orOperator = num1 - num2 == 2 || num1 + num2 == 2
+	fmt.Printf("(%t) \n", andOperator)
+	fmt.Printf("(%t) \n", orOperator)
+	fmt.Printf("(%t) \n", !orOperator)
+}
