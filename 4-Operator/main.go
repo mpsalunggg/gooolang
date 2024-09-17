479cc96d7c69bb261ebe2e58842fae742e4e3f48
mpsalunggg <putrasatria893@gmail.com>
2024-09-17 23:03:09 +0700

    fix: formatting code

--- 4-Operator/main.go
+++ 4-Operator/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// Operator Arithmetic 
-	const value = (((2 + 2) % 2) * 2 - 2) / 2
+	// Operator Arithmetic
+	const value = (((2+2)%2)*2 - 2) / 2
 	fmt.Println(value)
 
 	// Operator Comparison
@@ -12,11 +12,11 @@ func main() {
 	// != -> not equal
 	// etc same like js
 
-	var num1 = 1 
+	var num1 = 1
 	var num2 = 1
 
-	var andOperator = num1 + num1 == 2 && num1 + num2 == 1
-	var orOperator = num1 - num2 == 2 || num1 + num2 == 2
+	var andOperator = num1+num1 == 2 && num1+num2 == 1
+	var orOperator = num1-num2 == 2 || num1+num2 == 2
 	fmt.Printf("(%t) \n", andOperator)
 	fmt.Printf("(%t) \n", orOperator)
 	fmt.Printf("(%t) \n", !orOperator)
