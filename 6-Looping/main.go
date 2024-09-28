355dec712cac441fceb2fa55d389795cb84f9616
mpsalunggg <putrasatria893@gmail.com>
2024-09-29 00:48:02 +0700

    break, continue, label

--- 6-Looping/main.go
+++ 6-Looping/main.go
@@ -37,4 +37,30 @@ func main() {
 	for i, v := range xs {
     	fmt.Println("Index=", i, "Value=", v)
 	}
+
+	// Break & Continue
+	for i := 0; i < 10; i++ {
+		if i % 2 == 0 {
+			continue
+		}
+
+		if i > 8 {
+			break
+		}
+
+		fmt.Println("Num ", i)
+	}
+
+	// labeling
+	OuterLoop:
+    for i := 0; i < 3; i++ {
+        for j := 0; j < 3; j++ {
+            if j == 2 {
+                fmt.Println("Keluar dari OuterLoop")
+                break OuterLoop
+            }
+            fmt.Printf("i: %d, j: %d\n", i, j)
+        }
+    }
+    fmt.Println("Perulangan selesai")
 }
