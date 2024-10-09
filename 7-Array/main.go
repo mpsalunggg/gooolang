525f199f41fbcd8df67eb3bf7b05d7334dc04998
mpsalunggg <putrasatria893@gmail.com>
2024-10-09 22:22:09 +0700

    arrayy material

---
+++ 7-Array/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [4]string
+	names[0] = "trafalgar"
+	names[1] = "d"
+	names[2] = "water"
+	names[3] = "law"
+	// names[4] = "ez" // out off bound
+
+	// first initialization
+	var animal = [2]string{"monkey", "cow"}
+
+	fmt.Println("Jumlah element \t\t", len(animal))
+	fmt.Println("Isi semua element \t", animal)
+
+}
