6b6c065fdbb36b5427e3405e2ed4a16570738c16
mpsalunggg <putrasatria893@gmail.com>
2024-10-15 11:59:07 +0700

    relation array with slice

--- 8-Slice/main.go
+++ 8-Slice/main.go
@@ -9,4 +9,14 @@ func main() {
 
 	fmt.Println(newFruits)
 
+
+	// Relation Array with Slice
+	var new_fruits = []string{"apple", "grape", "banana", "melon"}
+	fmt.Println(new_fruits[0:2])
+	fmt.Println(new_fruits[4:4])
+	// fmt.Println(new_fruits[4:0]) error because 0 is < 4
+	fmt.Println(new_fruits[:]) //all data on array
+	fmt.Println(new_fruits[:2])
+	fmt.Println(new_fruits[2:])
+
 }
