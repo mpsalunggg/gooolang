7e7e728c68e8e4a17a8a3ff43c9703f7ba4e2bc8
mpsalunggg <putrasatria893@gmail.com>
2024-12-15 22:48:31 +0700

    function callback and rand new

--- 10-function/main.go
+++ 10-function/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func main() {
 	names := []string{"Putra", "Satria"}
 	callName("Haiii, ", names)
+	addNum := add(2, 3)
+	fmt.Println(addNum)
+	randomNew()
 }
 
 func callName(message string, names []string) string {
 	nameString := strings.Join(names, " ")
 	fmt.Println(message, nameString)
 	return nameString
+}
+
+// function with callback return value
+func add(a int, b int) int {
+	return a + b
+}
+
+// function rand.New()
+func randomNew() {
+	num := rand.New(rand.NewSource(time.Now().Unix()))
+	fmt.Println(num)
 }
