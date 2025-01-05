6d5349646dc832110f5fc73d6feecd9b0a653dab
mpsalunggg <putrasatria893@gmail.com>
2025-01-05 23:18:07 +0700

    pointer with change value parameter

--- 12-pointer/main.go
+++ 12-pointer/main.go
@@ -18,4 +18,16 @@ func main() {
 	fmt.Println("numberA:", &numberA)
 	fmt.Println("numberB:", *numberB)
 	fmt.Println("numberB:", numberB)
+	fmt.Println("======")
+	// Pointer with parameter
+	numberC := 4
+	fmt.Println("Before", numberC)
+	changeValue(&numberC)
+	fmt.Println("After", numberC)
+	fmt.Println("numberC:", &numberC)
+}
+
+// Pointer with parameter
+func changeValue(number *int) {
+	*number = 100
 }
