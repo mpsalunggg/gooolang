77108daf6c3396745afe073681378b5a1b860483
mpsalunggg <putrasatria893@gmail.com>
2025-01-12 00:01:30 +0700

    pointer with struct and method

--- 14-method/main.go
+++ 14-method/main.go
@@ -11,6 +11,11 @@ type People struct {
 	Age int
 }
 
+type Animal struct {
+	Name string
+	Type string
+}
+
 func (p People) sayHello() {
     fmt.Println("Hello", p.Name)
 }
@@ -19,10 +24,29 @@ func (p People) getFirstName(i int) string {
 	return strings.Split(p.Name, " ")[i - 1]
 }
 
+func (a Animal) changeName(n string){
+	a.Name = n
+	fmt.Println("Not change -> " , a.Name)
+}
+
+func (a *Animal) changeNamePointer(n string){
+	a.Name = n
+	fmt.Println("Change -> " , a.Name)
+}
+
 func main(){
 	people := People{"putra satria", 23}
 	people.sayHello()
 
 	firstName := people.getFirstName(1)
 	fmt.Println("First Name:", firstName)
+
+	fmt.Println("=====================================")
+
+	animal := Animal{"Dog", "Mammal"}
+	animal.changeName("Cat")
+	fmt.Println(animal.Name)
+	animal.changeNamePointer("Rabbit")
+	fmt.Println(animal.Name)
+
 }
