edd2b2ef685a28d15aa766a42e7790ede8f70f78
mpsalunggg <putrasatria893@gmail.com>
2025-01-18 23:42:56 +0700

    pointer interface

--- 16-interface/main.go
+++ 16-interface/main.go
@@ -38,6 +38,23 @@ func (p Persegi) keliling() float64 {
 	return p.sisi * 4
 }
 
+type Describer interface {
+	Describe() string
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) Describe() string {
+	return fmt.Sprintf("Name: %s, Age: %d", p.Name, p.Age)
+}
+
+func PrintDescription(d Describer) {
+	fmt.Println(d.Describe())
+}
+
 func main() {
 	// INTERFACE
 
@@ -53,4 +70,14 @@ func main() {
 	fmt.Println("luas      :", bangunDatar.luas())
 	fmt.Println("keliling  :", bangunDatar.keliling())
 	fmt.Println("jari-jari :", bangunDatar.(Lingkaran).jariJari())
+
+	fmt.Println("================================")
+	// Example 2
+	p := &Person{Name: "Putra", Age: 23}
+	PrintDescription(p)
+
+	p.Name = "Test"
+	p.Age = 24
+
+	PrintDescription(p)
 }
