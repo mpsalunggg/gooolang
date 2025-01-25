5511d7124f595dc8b785f50bf9e922a7841aa27d
mpsalunggg <putrasatria893@gmail.com>
2025-01-25 11:27:35 +0700

    another any interface

--- 17-interface-any/main.go
+++ 17-interface-any/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// Interface any
@@ -13,10 +16,10 @@ func main() {
 
 	secret = []string{"apple", "mangga", "pineapple"}
 
-	fmt.Println(secret)
+	var gruits = strings.Join(secret.([]string), ", ")
+	fmt.Println(gruits, "is my favorite fruits")
 
-	var data map[string]interface{}
-	data = map[string]interface{}{
+	data := map[string]interface{}{
 		"name":   "mps",
 		"grade":  2,
 		"fruits": []string{"apple", "manggo", "banana"},
@@ -24,7 +27,9 @@ func main() {
 
 	fmt.Println(data)
 
-	var data2 map[string]any
+	data2 := map[string]any{}
+
+	fmt.Println(data2)
 	data2 = map[string]any{
 		"name":      "ethan hunt",
 		"grade":     2,
@@ -32,4 +37,18 @@ func main() {
 	}
 
 	fmt.Println(data2)
+	data2["say"] = "hallooo"
+	fmt.Println(data2["say"])
+
+	fmt.Println("=======================")
+	var fruits = []interface{}{
+		map[string]interface{}{"name": "strawberry", "total": 10},
+		[]string{"manggo", "pineapple", "papaya"},
+		"orange",
+	}
+
+	for _, each := range fruits {
+		fmt.Println(each)
+	}
+
 }
