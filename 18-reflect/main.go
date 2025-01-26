05fbd30db3aac204ab3ec07cc8b960b73ff6303d
mpsalunggg <putrasatria893@gmail.com>
2025-01-26 21:45:14 +0700

    access object with reflect

--- 18-reflect/main.go
+++ 18-reflect/main.go
@@ -5,7 +5,12 @@ import (
 	"reflect"
 )
 
-func main(){
+type Student struct {
+	Name string
+	Age  int
+}
+
+func main() {
 	number := 23
 	reflectValue := reflect.ValueOf(number)
 
@@ -14,10 +19,32 @@ func main(){
 	fmt.Println(reflectValue.Int())
 
 	if reflectValue.Kind() == reflect.Int {
-        fmt.Println("nilai variabel :", reflectValue.Int())
-    }
+		fmt.Println("nilai variabel :", reflectValue.Int())
+	}
 
 	// Access with interface casting
-	fmt.Println("interface : ",reflectValue.Interface().(int))
+	fmt.Println("interface : ", reflectValue.Interface().(int))
 
+	fmt.Println("===========================")
+	// Access with object
+	var s1 = &Student{Name: "wick", Age: 2}
+	s1.getProperty()
+}
+
+func (s *Student) getProperty() {
+	var reflectValue = reflect.ValueOf(s)
+	fmt.Println(reflectValue)
+	if reflectValue.Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+		fmt.Println("elem : ", reflectValue)
+	}
+
+	var reflectType = reflectValue.Type()
+	fmt.Println("type = ", reflectType)
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fmt.Println("nama      :", reflectType.Field(i).Name)
+		fmt.Println("tipe data :", reflectType.Field(i).Type)
+		fmt.Println("nilai     :", reflectValue.Field(i).Interface())
+		fmt.Println("")
+	}
 }
