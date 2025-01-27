6c9347388d4548d0ce738d36b0ff0ce7452902a0
mpsalunggg <putrasatria893@gmail.com>
2025-01-27 20:27:20 +0700

    method call reflect

--- 18-reflect/main.go
+++ 18-reflect/main.go
@@ -29,6 +29,16 @@ func main() {
 	// Access with object
 	var s1 = &Student{Name: "wick", Age: 2}
 	s1.getProperty()
+
+	fmt.Println("current name : ", s1.Name)
+	s1.SetName("Putra")
+
+	reflectValue2 := reflect.ValueOf(s1)
+	method := reflectValue2.MethodByName("SetName")
+	method.Call([]reflect.Value{
+		reflect.ValueOf("putra"),
+	})
+	fmt.Println("updated name : ", s1.Name)
 }
 
 func (s *Student) getProperty() {
@@ -47,4 +57,8 @@ func (s *Student) getProperty() {
 		fmt.Println("nilai     :", reflectValue.Field(i).Interface())
 		fmt.Println("")
 	}
+}
+
+func (s *Student) SetName(name string) {
+	s.Name = name
 }
