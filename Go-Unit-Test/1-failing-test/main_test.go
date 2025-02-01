7ae7633428c7ea8ac4c112f56928c4e992bf3b34
mpsalunggg <putrasatria893@gmail.com>
2025-02-01 23:16:04 +0700

    testing fail, failnow, and fatal

--- Go-Unit-Test/1-failing-test/main_test.go
+++ Go-Unit-Test/1-failing-test/main_test.go
@@ -1,11 +1,50 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
-	result := Add(1, 3)
+	result := Add(1, 2)
 
 	if result != 4 {
-		t.Errorf("Number should be same the result")
+		t.Error("Number should be same the result")
 	}
+
+	// this will be executed when the test is passed
+	fmt.Println("Test Add passed")
+}
+
+func TestAdd2(t *testing.T) {
+	result := Add(1, 2)
+
+	if result != 4 {
+		t.Fatal("Number should be same the result")
+	}
+
+	// this will be executed when the test is failed
+	fmt.Println("Test Add2 passed")
+}
+
+func TestAdd3(t *testing.T) {
+	result := Add(1, 2)
+
+	if result != 4 {
+		t.Fail()
+	}
+
+	// this will be executed when the test is failed
+	fmt.Println("Test Add3 passed")
+}
+
+func TestAdd4(t *testing.T) {
+	result := Add(1, 2)
+
+	if result != 4 {
+		t.FailNow()
+	}
+
+	// this not will be executed when the test is failed
+	fmt.Println("Test Add4 passed")
 }
