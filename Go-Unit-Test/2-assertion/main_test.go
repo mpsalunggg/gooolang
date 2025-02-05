0bf96cfde37b8c7639e1d8b6fd78079981b77784
mpsalunggg <putrasatria893@gmail.com>
2025-02-05 14:02:36 +0700

    require testing with testify

--- Go-Unit-Test/2-assertion/main_test.go
+++ Go-Unit-Test/2-assertion/main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSayHelloPeople(t *testing.T) {
@@ -12,5 +14,17 @@ func TestSayHelloPeople(t *testing.T) {
 
 	assert.Equal(t, "Hello, "+exampleName, result, "Result should be Hello, "+exampleName)
 	assert.Contains(t, result, "Hello, "+exampleName, "Result should contain Hello, "+exampleName)
-	// assert.NotContains(t, result, "Hello, "+exampleName, "Result should not contain Hello, "+exampleName)
+
+	// if the assertion is not passed, the test will be stopped and the message will be printed
+	fmt.Println("TestSayHelloPeople with assertion passed")
+}
+
+func TestSayHelloPeopleWithRequire(t *testing.T) {
+	exampleName := "Yuhu"
+	result := SayHelloPeople(exampleName)
+
+	require.Equal(t, "Hello, "+exampleName, result, "Result should be Hello, "+exampleName)
+
+	// if the require is not passed, the test will be stopped and the message will be printed
+	fmt.Println("TestSayHelloPeopleWithRequire passed")
 }
