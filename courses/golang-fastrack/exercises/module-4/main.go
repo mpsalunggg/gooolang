07b306fc9c1d9de486f3178f2fc15c6813e29fd3
mpsalunggg <putrasatria893@gmail.com>
2025-12-01 21:58:39 +0700

    exercise module 4

--- courses/golang-fastrack/exercises/module-4/main.go
+++ courses/golang-fastrack/exercises/module-4/main.go
@@ -20,11 +20,37 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Nama: %s\n", nama)
 }
 
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK) // 200
+	w.Write([]byte("OK"))
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound) // 404
+	w.Write([]byte("Not Found"))
+}
+
+func badRequestHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest) // 400
+	w.Write([]byte("Bad Request"))
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK) // 200
+	w.Write([]byte("Selamat datang di API"))
+	nama := r.URL.Query().Get("nama")
+	w.Write([]byte("Hello bapak: " + nama))
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/ok", okHandler)
+	http.HandleFunc("/notfound", notFoundHandler)
+	http.HandleFunc("/badrequest", badRequestHandler)
+	http.HandleFunc("/welcome", welcomeHandler)
 
-	fmt.Println("Server berjalan di http://localhost:8080")
+	fmt.Println("Server berjalan di http://localhost:8181")
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8181", nil)
 }
