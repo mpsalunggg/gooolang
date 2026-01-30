6f394da02e872d1c6bd465d6b3f07de3ff7eeb16
mpsalunggg <putrasatria893@gmail.com>
2026-01-30 21:57:11 +0700

    range channel and select channel

--- pzn/5-Goroutine/channel_test.go
+++ pzn/5-Goroutine/channel_test.go
@@ -122,3 +122,56 @@ func TestBufferedChannel(t *testing.T) {
 
 	// time.Sleep(4 * time.Second)
 }
+
+// Range Channel
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		channel <- "Hello"
+		channel <- "World"
+		channel <- "Golang"
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println(data)
+	}
+
+	time.Sleep(2 * time.Second)
+}
+
+// Select Channel
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel1 <- "Hello"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel2 <- "World"
+	}()
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
