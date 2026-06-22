b500ae769ed44a9415d88b4edb5406008b9c0042
mpsalunggg <putrasatria893@gmail.com>
2026-06-22 21:55:45 +0700

    golang web http server with handler

---
+++ pzn/9-Web/handler_test.go
@@ -0,0 +1,55 @@
+package goweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Helloo world!")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8181",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMuxHandler(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Home")
+	})
+
+	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About")
+	})
+
+	mux.HandleFunc("/images", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Images")
+	})
+ 
+	mux.HandleFunc("/images/thumbnail", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8181",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
