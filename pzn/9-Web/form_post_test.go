c0c81d7015a29d26a1107a22c8c09c52d735028d
mpsalunggg <putrasatria893@gmail.com>
2026-06-29 21:33:45 +0700

    form post

---
+++ pzn/9-Web/form_post_test.go
@@ -0,0 +1,39 @@
+package goweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := r.PostForm.Get("first_name")
+	lastName := r.PostForm.Get("last_name")
+
+	fmt.Fprintf(w, "%s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=mps&last_name=yuhu")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost/", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
+
