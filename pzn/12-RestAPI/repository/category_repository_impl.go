aeb8c5d34d3d903ad407843afb4a17def774c9e6
mpsalunggg <putrasatria893@gmail.com>
2026-07-27 21:30:45 +0700

    helper panic err, dan finishing query repository

--- pzn/12-RestAPI/repository/category_repository_impl.go
+++ pzn/12-RestAPI/repository/category_repository_impl.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"restapi/helper"
 	"restapi/model/domain"
 )
 
@@ -10,21 +12,60 @@ type CategoryRepositoryImpl struct {
 }
 
 func (r *CategoryRepositoryImpl) Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category {
-	panic("not implemented") // TODO: Implement
+	query := "INSERT INTO customer(name) values (?)"
+	result, err := tx.ExecContext(ctx, query, category.Name)
+
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
 }
 
-func (r *CategoryRepositoryImpl) Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category {
-	panic("not implemented") // TODO: Implement
+func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "UPDATE category SET name = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfError(err)
+
+	return category
 }
 
-func (r *CategoryRepositoryImpl) Delete(ctx context.Context, tx sql.Tx, category domain.Category) {
-	panic("not implemented") // TODO: Implement
+func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	SQL := "DELETE from category WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+	helper.PanicIfError(err)
 }
 
-func (r *CategoryRepositoryImpl) FindById(ctx context.Context, tx sql.Tx, categoryId int) domain.Category {
-	panic("not implemented") // TODO: Implement
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "SELECT id, name FROM category WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
 }
 
-func (r *CategoryRepositoryImpl) FindAll(ctx context.Context, tx sql.Tx) []domain.Category {
-	panic("not implemented") // TODO: Implement
+func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "SELECT id, name FROM category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		categories = append(categories, category)
+	}
+	return categories
 }
